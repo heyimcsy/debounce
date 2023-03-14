4b6005443abe2177df77055d799601ee2e1d60d6
최서윤 <cinnamon1995@naver.com>
2023-03-14 19:41:59 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState, useCallback } from 'react'
+import _ from 'lodash'
 
 function App() {
+  const [searchText, setSearchText] = useState('')
+  const [inputText, setInputText] = useState('')
+
+  const debounce = (callback, delay) => {
+    let timerId = null
+    return (...args) => {
+      if (timerId) clearTimeout(timerId)
+      timerId = setTimeout(() => {
+        callback(...args)
+      }, [delay])
+    }
+  }
+
+  const handleSearchText = useCallback(
+    debounce((text) => {
+      setSearchText(text)
+    }, 1000),
+    []
+  )
+
+  //공통함수
+  const handleChange = (e) => {
+    setInputText(e.target.value)
+    handleSearchText(e.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        paddingLeft: '20px',
+        paddingRight: '20px',
+      }}
+    >
+      <h1>디바운싱 예제</h1>
+      <input
+        placeholder="입력값을 넣고 디바운싱 테스트를 해보세요"
+        style={{ width: '300px' }}
+        type="text"
+        onChange={handleChange}
+      />
+      <p>Search Text : {searchText} </p>
+      <p>Input Text : {inputText}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
